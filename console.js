6fbe0571fc8f073b3b9eb919c55f0d98b5e75c5f
sammybrabra132 <123180906+sammybrabra132@users.noreply.github.com>
2023-01-22 21:55:10 +0700

    Delete console.js

--- console.js
+++
@@ -1,1 +0,0 @@
-document.getElementById("video").innerHTML = "<iframe width=\"800\" height=\"400\" src=\"https://www.youtube.com/embed/oA8VrcQYozI\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>"
